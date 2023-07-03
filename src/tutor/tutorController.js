577b4a53298030a92f415fb92f05a5fdf1bd9f4d
eomgerm <fishbread00@gmail.com>
2023-07-04 01:08:06 +0900

    hotfix: set to json structure

--- src/tutor/tutorController.js
+++ src/tutor/tutorController.js
@@ -1,10 +1,11 @@
+import { response } from "../../config/response";
 import tutorService from "./tutorService";
 
 const tutorController = {
   getTutors: async (req, res) => {
     const { beach_id, day, time, level } = req.body;
 
-    res.json(await tutorService.getTutors(beach_id, day, time, level));
+    res.json(response(await tutorService.getTutors(beach_id, day, time, level)));
   },
 };
 
