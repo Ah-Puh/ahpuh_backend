9eef3c243debb146c32d178299cf75786cb71690
Yuunhye <pyl66600@gmail.com>
2023-07-04 02:27:15 +0900

    feat: BE-9 /reservation/:lecture_id

--- src/reservation/reservationRouter.js
+++ src/reservation/reservationRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
+const reservation = require('./reservationController');
 
 const reservationRouter = express.Router();
+
+reservationRouter.post('/:lecture_id', reservation.postReservation);
 
 export default reservationRouter;
